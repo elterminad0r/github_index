ce22ff63fd95cab3c54535eaebc91e32e2370e67
Izaak van Dongen <izaak.van.dongen@gmail.com>
2017-09-10 15:12:34 +0100

    added toc

--- format_readme.py
+++ format_readme.py
@@ -3,16 +3,26 @@ A bit of a meta-project on creating a nicer index of my github repositories. Par
 """
 
 import sys
-import re
 
 sys.stdout.write(__doc__)
+
+out = []
+toc = []
 
 for line in sys.stdin:
     if line.strip():
         if line.startswith("#"):
-            sys.stdout.write("#{}".format(line))
+            out.append("#{}".format(line))
+            toc.append("1. [{}](#{})\n".format(line[2:-1], line[2:-1].lower().replace(" ", "-")))
         else:
             rep_dir, rep_name, rep_url, rep_desc = line.strip().split("\t")
             with open(rep_dir + "README.md", "r") as rep_file:
                 for r_line in rep_file:
-                    sys.stdout.write(re.sub(r"^# (.*)$", r"### [\1]({})".format(rep_url), r_line))
+                    if r_line.startswith("#"):
+                        out.append("### [{}]({})\n".format(r_line[2:-1], rep_url))
+                        toc.append("    1. [{}](#{})\n".format(r_line[2:-1], r_line[2:-1].lower().replace(" ", "-")))
+                    else:
+                        out.append(r_line)
+
+sys.stdout.writelines(toc)
+sys.stdout.writelines(out)
